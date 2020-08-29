ae9183f7235dd43f2aa3f9414c6091362c714abf
goll123 <58493365+goll123@users.noreply.github.com>
2020-08-29 01:39:14 -0600

    Add files via upload

---
+++ src/main/java/gt/com/proyectoanalisis2/model/DatosCajero.java
@@ -0,0 +1,14 @@
+package gt.com.proyectoanalisis2.model;
+
+public class DatosCajero {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+   }
